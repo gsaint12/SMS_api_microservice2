818311d0b3b3a12ed286350a19b0608c623fc64a
Godsent Clement <gsnigeriatech@gmail.com>
2020-09-19 18:00:11 +0000

    Complete working template

---
+++ Readme.md
@@ -0,0 +1,5 @@
+the first steps taken:-
+Install Node.js through the official link
+creat a react app from the command line named client
+creat an Express api server named api
+
---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state ={ apiResponse:"" };
+  }
+  callAPI () {
+    fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
+        
+  }
+  componentDidMount() {
+    this.callAPI();
+}
+
+render() {
+  return(
+    <div className = "App">
+        <header className= "App-header">
+          <img src= {logo} className= "App-logo" alt= "logo" />
+          <h1 className= "App-title"> My Fisrt API App </h1>
+        </header>
+    <p className= "App-intro" >{this.state.apiResponse} </p>
+        
+    </div>
+  );
+}
+
+}
+
+export default App;
